f7bf583f1ffefc4d02dbed9e2bd1852a799c146e
Shubham Patel <shubhamashishpatel@gmail.com>
2022-10-05 22:59:30 +0530

    Update nftContract.sol

--- nftContract.sol
+++ nftContract.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
+// URI : https://ipfs.filebase.io/ipfs/QmQvxS3Rm6fcnTuF6VyKv4rSGVXPCReqtiiTcrknLzScH4
 import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts@4.7.3/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -13,10 +13,16 @@ contract StalkNftSEarlyUser is ERC721, ERC721Enumerable, ERC721URIStorage{
     Counters.Counter private _tokenIdCounter;
     uint256 MAX_SUPPLY = 1000;
     constructor() ERC721("Stalk Nft's Early User", "Stlk") {}
-
+    struct counter{
+        uint256 mintedNfts;
+    }
+    mapping(address => counter) mintCounter;
     function safeMint(address to, string memory uri) public{
         uint256 tokenId = _tokenIdCounter.current();
+
        require(tokenId<MAX_SUPPLY,"You have reached max supply,sry");
+       mintCounter[msg.sender].mintedNfts += 1;
+       require(mintCounter[msg.sender].mintedNfts<=5,"You have reached max minting limit ");
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
